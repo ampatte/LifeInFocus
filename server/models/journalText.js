cb307f13b4a30f679f41f53766b4782ae3b1bd62
unknown <harrisondjon1994@gmail.com>
2023-04-05 18:11:44 -0700

    made dolers and files for the server, added code to models and config files

---
+++ server/models/journalText.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+
+// type Date {
+//     id: String,
+//     date: String,
+//     items: [JournalEntry]
+// }
+
+// type JournalEntry {
+//     id: ID,
+//     date: String,
+//     text: String, 
+//     lastUpdated: String
+// }
+
