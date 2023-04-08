293a769322ec420030ed99b52b3acd773fe62c73
nico <westernfarthing@gmail.com>
2023-04-07 22:45:36 -0500

    added quote json file

--- client/src/components/Rotating/Rotating.js
+++ client/src/components/Rotating/Rotating.js
@@ -4,6 +4,8 @@ import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
 import { createTheme, ThemeProvider, styled } from "@mui/material/styles";
 
+import quotes from "../../data/quotes.json";
+
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
   textAlign: "center",
@@ -15,16 +17,17 @@ const Item = styled(Paper)(({ theme }) => ({
 function Rotating() {
   return (
     <Grid container spacing={2}>
-    <Grid item xs={12} sm={12}>
-      <Paper elevation={10} sx={{ padding: 2 }}>
-        <h2>mememe of the Day</h2>
-        <p>
-          akjdajdwbkjdbjwd adwjkadb wad jadkjwdbahd ahd hawdba da dahdwhdkjba
-          dakhbd
-        </p>
-      </Paper>
+      <Grid item xs={12} sm={12}>
+        <Paper elevation={10} sx={{ padding: 2 }}>
+          <h2>Motivational Quote of the Day</h2>
+          <div classname="quote-wrapper"></div>
+          {/*Loop through the quotes.json and render each quote dynamically*/}
+          {quotes.map((quote) => {
+            return <div key={quote.id}>{quote.text}</div>;
+          })}
+        </Paper>
+      </Grid>
     </Grid>
-  </Grid>
   );
 }
 export default Rotating;
